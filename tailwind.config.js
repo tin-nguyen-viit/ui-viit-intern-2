182b31339c5efd53da2268b4dfdb458374553d2c
NguyenDucTin <99305494+Yonko08042002@users.noreply.github.com>
2024-01-16 17:00:06 +0700

    create 60% project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "vng-bgr": "url('pubic/imgs/earth-1681722_960_720.png')",
+      },
+    },
+  },
+  plugins: [],
+}
